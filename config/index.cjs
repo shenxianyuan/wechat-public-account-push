841a2d9f09193f364e4eb5d85e5ead175861153a
shenxianyuan <65546132+shenxianyuan@users.noreply.github.com>
2022-09-30 19:10:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oT5K45shSgdJXAtE-vMdVNL_NQGg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FFQOwOY2EJbQuJf6o2mVGLeQ0BYSDBMKl6Dbc_rIvJ4',
+      useTemplateId: '2DIMAmCb_ht113p8Td8Wmavrht-LVs2BSAdXyt89TII',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-18',
       festivals: [
